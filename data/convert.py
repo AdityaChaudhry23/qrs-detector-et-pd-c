32ebc4ec2a9468ccad98379adb267843a4df7575
Aditya Chaudhry <adityachaudhry200323@gmail.com>
2025-07-19 17:09:46 +0100

    Starting a fresh project with a better idea

--- data/convert.py
+++
@@ -1,7 +0,0 @@
-import wfdb, pandas as pd
-
-record = wfdb.rdrecord('203')
-pd.DataFrame(record.p_signal, columns=record.sig_name).to_csv('203.csv', index=False)
-
-ann = wfdb.rdann('203', 'atr')
-pd.DataFrame({'sample': ann.sample, 'symbol': ann.symbol}).to_csv('203.ann.csv', index=False)
